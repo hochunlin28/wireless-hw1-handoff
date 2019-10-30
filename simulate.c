2913363e1fd010904af2607f5f22febd2e6340e3
F74064096 <john28152815@gmail.com>
2019-10-30 12:14:18 +0800

    fix car turn() problem

--- simulate.c
+++ simulate.c
@@ -11,38 +11,38 @@ void init()
 {
     spawn_prob = (1/30.0)*exp(-1/30.0);
     //printf("%f\n",spawn_prob);
-    BS_array[0].x = 600*9;
-    BS_array[0].y = 600*9;
-    BS_array[1].x = 1800*9;
-    BS_array[1].x = 600*9;
-    BS_array[2].x = 600*9;
-    BS_array[2].y = 1800*9;
-    BS_array[3].y = 1800*9;
-    BS_array[3].y = 1800*9;
-    entry_array[0].x = 600*9;
-    entry_array[0].y = 0*9;
-    entry_array[1].x = 1200*9;
-    entry_array[1].y = 0*9;
-    entry_array[2].x = 1800*9;
-    entry_array[2].y = 0*9;
-    entry_array[3].x = 0*9;
-    entry_array[3].y = 600*9;
-    entry_array[4].x = 0*9;
-    entry_array[4].y = 1200*9;
-    entry_array[5].x = 0*9;
-    entry_array[5].y = 1800*9;
-    entry_array[6].x = 600*9;
-    entry_array[6].y = 2400*9;
-    entry_array[7].x = 1200*9;
-    entry_array[7].y = 2400*9;
-    entry_array[8].x = 1800*9;
-    entry_array[8].y = 2400*9;
-    entry_array[9].x = 2400*9;
-    entry_array[9].y = 1800*9;
-    entry_array[10].x = 2400*9;
-    entry_array[10].y = 1200*9;
-    entry_array[11].x = 2400*9;
-    entry_array[11].y = 600*9;
+    BS_array[0].x = BLOCK;
+    BS_array[0].y = BLOCK;
+    BS_array[1].x = BLOCK*3;
+    BS_array[1].x = BLOCK;
+    BS_array[2].x = BLOCK;
+    BS_array[2].y = BLOCK*3;
+    BS_array[3].y = BLOCK*3;
+    BS_array[3].y = BLOCK*3;
+    entry_array[0].x = BLOCK;
+    entry_array[0].y = 0;
+    entry_array[1].x = BLOCK*2;
+    entry_array[1].y = 0;
+    entry_array[2].x = BLOCK*3;
+    entry_array[2].y = 0;
+    entry_array[3].x = 0;
+    entry_array[3].y = BLOCK;
+    entry_array[4].x = 0;
+    entry_array[4].y = BLOCK*2;
+    entry_array[5].x = 0;
+    entry_array[5].y = BLOCK*3;
+    entry_array[6].x = BLOCK;
+    entry_array[6].y = BLOCK*4;
+    entry_array[7].x = BLOCK*2;
+    entry_array[7].y = BLOCK*4;
+    entry_array[8].x = BLOCK*3;
+    entry_array[8].y = BLOCK*4;
+    entry_array[9].x = BLOCK*4;
+    entry_array[9].y = BLOCK*3;
+    entry_array[10].x = BLOCK*4;
+    entry_array[10].y = BLOCK*2;
+    entry_array[11].x = BLOCK*4;
+    entry_array[11].y = BLOCK;
 }
 
 void simulate_car()
@@ -61,6 +61,9 @@ void simulate_car()
             car_num++;
             car_tmp->x = entry_array[i].x;
             car_tmp->y = entry_array[i].y;
+            car_tmp->BS_current = entry_BS[i];
+            car_tmp->power = Px;
+            car_tmp->time = 0;
             car_tmp->next = NULL;
             switch(i/3)
             {
@@ -105,7 +108,7 @@ void check_boundary()
     cur = NULL;
     for(cur = car_head; cur != NULL;)
     {
-        if(cur->x >= 2400*9 || cur->x <= 0 || cur->y >= 2400*9 ||cur->y <= 0 )  //out of boundary
+        if(cur->x >= BLOCK*4 || cur->x <= 0 || cur->y >= BLOCK*4 ||cur->y <= 0 )  //out of boundary
         {
             car_num--;
             if(cur == car_head)
@@ -132,69 +135,65 @@ void check_boundary()
     }
 }
 
-void turn()
+void turn(struct Car *tmp)
 {
-    struct Car *tmp = NULL;
-    for(tmp = car_head; tmp !=NULL; tmp = tmp->next)
+    r_turn = (float)rand()/RAND_MAX;
+    printf("%f %f time=%d\n",tmp->x,tmp->y,tmp->time);
+    switch (tmp->direction_x)
     {
-        r_turn = (float)rand()/RAND_MAX;
-        //printf("%f\n",r_turn);
-        switch (tmp->direction_x)
+    case -1://left
+        if(r_turn>0.5 && r_turn<=0.5+(1/3.0))
         {
-        case -1://left
-            if(r_turn>0.5 && r_turn<=0.5+(1/3.0))
-            {
-                tmp->direction_x = 0;
-                tmp->direction_y = -1;
-            }
-            else if(r_turn>0.5+(1/3.0))
-            {
-                tmp->direction_x = 0;
-                tmp->direction_y = 1;
-            }
-            break;
-        case 1: //right
+            tmp->direction_x = 0;
+            tmp->direction_y = -1;
+        }
+        else if(r_turn>0.5+(1/3.0))
+        {
+            tmp->direction_x = 0;
+            tmp->direction_y = 1;
+        }
+        break;
+    case 1: //right
+        if(r_turn>0.5 && r_turn<=0.5+(1/3.0))  //turn right
+        {
+            tmp->direction_x = 0;
+            tmp->direction_y = 1;
+        }
+        else if(r_turn>0.5+(1/3.0))  //turn left
+        {
+            tmp->direction_x = 0;
+            tmp->direction_y = -1;
+        }
+        break;
+    case 0:
+        if(tmp->direction_y==1)  //down
+        {
             if(r_turn>0.5 && r_turn<=0.5+(1/3.0))  //turn right
             {
-                tmp->direction_x = 0;
-                tmp->direction_y = 1;
+                tmp->direction_x = -1;
+                tmp->direction_y = 0;
             }
             else if(r_turn>0.5+(1/3.0))  //turn left
             {
-                tmp->direction_x = 0;
-                tmp->direction_y = -1;
+                tmp->direction_x = 1;
+                tmp->direction_y = 0;
             }
-            break;
-        case 0:
-            if(tmp->direction_y==1)  //down
-            {
-                if(r_turn>0.5 && r_turn<=0.5+(1/3.0))  //turn right
-                {
-                    tmp->direction_x = -1;
-                    tmp->direction_y = 0;
-                }
-                else if(r_turn>0.5+(1/3.0))  //turn left
-                {
-                    tmp->direction_x = 1;
-                    tmp->direction_y = 0;
-                }
 
-            }
-            else  //up
-            {
-                if(r_turn>0.5 && r_turn<=0.5+(1/3.0))  //turn right
-                {
-                    tmp->direction_x = 1;
-                    tmp->direction_y = 0;
-                }
-                else if(r_turn>0.5+(1/3.0))  //turn left
-                {
-                    tmp->direction_x = -1;
-                    tmp->direction_y = 0;
-                }
-            }
-            break;
         }
+        else  //up
+        {
+            if(r_turn>0.5 && r_turn<=0.5+(1/3.0))  //turn right
+            {
+                tmp->direction_x = 1;
+                tmp->direction_y = 0;
+            }
+            else if(r_turn>0.5+(1/3.0))  //turn left
+            {
+                tmp->direction_x = -1;
+                tmp->direction_y = 0;
+            }
+        }
+        break;
     }
 }
 
@@ -203,31 +202,35 @@ void move()
     struct Car *tmp;
     for(tmp=car_head; tmp!=NULL; tmp=tmp->next)
     {
-        tmp->x = tmp->x + 100*tmp->direction_x;
-        tmp->y = tmp->y + 100*tmp->direction_y;
-        //		printf("%f %f\n",(double)tmp->x/9, (double)tmp->y/9);
+        tmp->time = tmp->time+1;
+        tmp->x = tmp->x + velocity*tmp->direction_x;
+        tmp->y = tmp->y + velocity*tmp->direction_y;
+        if(tmp->time % 75 == 0)
+        {
+            turn(tmp);
+        }
     }
 }
+void calculate_Best()
+{
+    struct Car *tmp;
+    for(tmp=car_head; tmp!=NULL; tmp=tmp->next)
+    {
+        //		printf("%f %f\n",(double)tmp->x/9, (double)tmp->y/9);
+    }
 
+}
 int main(void)
 {
     struct Car *car_tmp;
     int i;
     srand(time(NULL));
     init();
-    for(i=1; i<=54*6; i++)
+    for(i=1; i<=75+8; i++)
     {
         simulate_car();
         move(); //car move
         check_boundary();// check if car is out of boundary
-
-        if(i%54 == 0)
-        {
-            //printf("%d\n",car_num);
-            //printf("%f %f\n",(double)car_tmp->x/9, (double)car_tmp->y/9);
-            turn();
-        }
-
     }
     return 0;
 }
--- simulate.h
+++ simulate.h
@@ -1,9 +1,9 @@
 #ifndef SIMULATE_H
 #define SIMULATE_H
 
-#define Px -60 //less than 1m
-#define velocity_change 100
-#define velocity_real 100/9
+#define Px -50 //less than 1m
+#define velocity 10
+#define BLOCK 750
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -11,23 +11,26 @@
 //real distance is (x or y)/9
 struct BS
 {
-    int x;
-    int y;
+    double x;
+    double y;
 };
 struct Entry
 {
-    int x;
-    int y;
+    double x;
+    double y;
 };
 
 struct Car
 {
-    int  x;
-    int  y;
+    double  x;
+    double  y;
     int direction_x;
     int direction_y;
+    double power;
+    int BS_current; //current BS
+    int time; //car exist time
     struct Car *next;
 };
 
-int entry_bs[12] = {0,0,1,0,2,2,2,3,3,3,1,1};
+int entry_BS[12] = {0,0,1,0,2,2,2,3,3,3,1,1};
 #endif  //ifndef COM_APP_H
